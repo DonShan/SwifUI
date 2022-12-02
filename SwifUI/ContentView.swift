a5aed5dcf83e885d9d1098f8a67d9a48e63d20d8
DonShan <msdjshan47@gmail.com>
2022-12-02 21:05:17 +0530

    Initial Commit

---
+++ SwifUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwifUI
+//
+//  Created by Madushan Senavirathna on 2022-12-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwifUI/SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIApp.swift
+//  SwifUI
+//
+//  Created by Madushan Senavirathna on 2022-12-02.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
