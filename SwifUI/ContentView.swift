306c47d9b454f3c2617ea5ca85bd349bbe84c147
DonShan <msdjshan47@gmail.com>
2022-12-08 17:01:27 +0530

    We Split app

--- SwifUI/ContentView.swift
+++ SwifUI/ContentView.swift
@@ -8,12 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeolple = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountFocused: Bool
+    
+    let tipPercentages: [Int] = [10, 15, 20, 25, 0]
+    
+    var amountPerPerson: Double {
+        let peopleCount = Double(numberOfPeolple + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeolple) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                } header: {
+                    Text("How much tip do you want to levae?")
+                }
+                Section {
+                    Text(amountPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+            }
+            .navigationTitle("We Split")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Spacer()
+                    Button("Done") {
+                        amountFocused = false
+                    }
+                }
+            }
+        }
+       
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
